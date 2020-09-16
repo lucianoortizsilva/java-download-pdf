a25ed5de8fbff93861ed3293513ab3e2a969b740
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-09-16 09:55:07 -0300

    Criando esqueleto do projeto spring boot

---
+++ src/test/java/com/lucianoortizsilva/JavaDownloadPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDownloadPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
